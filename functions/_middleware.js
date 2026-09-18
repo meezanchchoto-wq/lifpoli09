851f8a71643b055913f13e17d23838f6fefbb30a
meezanchchoto-wq <meezanchchoto@gmail.com>
2026-09-18 10:48:20 +0500

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -12,9 +12,9 @@ export async function onRequest(context) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    <meta property="og:title" content="❤️mega tube 🧡">
+    <meta property="og:title" content="❤️mega tube ">
     <meta property="og:description" content="">
-    <meta property="og:image" content="https://web.facebook.com/photo/?fbid=122103882159476058&set=a.122103882351476058">
+    <meta property="og:image" content="<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02A9qMPf9vGv8isXj3wsT6phAhEjBe6B2986hfq13kA3f9chexyxsWzySKVyYES1acl%26id%3D61594281759518&show_text=true&width=500" width="500" height="262" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>">
     <meta property="og:url" content="https://www.google.com">
     <meta property="og:type" content="website">
 </head>
